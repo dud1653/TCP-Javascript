1569e7fab23fc205cd8abe3ef9156dfd0595aada
Lee <dlehdud1653@kakao.com>
2020-08-25 22:29:31 +0900

    TCP-Javascript

---
+++ program.js
@@ -0,0 +1,20 @@
+// 자바스크립트의 실행문은 세미콜론(;)으로 구분
+var x = 10;
+var result = x + 3;
+
+// 대소문자를 구분
+var javascript = 10;
+var Javascript = 20;
+
+// 리터럴 - 직접 표현되는 값 그 자체
+var a = 12 // 숫자리터럴
+var b = 'Javascript' // 문자열 리터럴
+var c = true // 불리언 리터럴
+
+// 식별자 작성 방식
+// 1. Camel Case 방식
+var firstVar = 10;
+
+// 2. Underscroe Case 방식
+var first_var = 20;
+
